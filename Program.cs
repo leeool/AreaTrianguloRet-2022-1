798905acceae03e1b9fb0691f44044bfadfc6ff7
leeool <lgonsalez02@gmail.com>
2022-03-25 22:19:29 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double Base, altura;
+
+Console.Write("Entre com a base: ");
+Base = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entre com a altura: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Area...: {(altura * Base) / 2:N2}");
